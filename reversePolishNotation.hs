8f23468356c87a3114c5600ba186ef42d55ec64c
Lifu Huang <lifu.hlf@gmail.com>
2017-09-06 23:16:44 -0700

    Support more operators in RPN.

--- reversePolishNotation.hs
+++ reversePolishNotation.hs
@@ -1,9 +1,12 @@
 import Data.List
 
-solveRPN :: (Fractional a, Num a, Read a) => String -> a
+solveRPN :: (Floating a, Num a, Read a) => String -> a
 solveRPN = head . foldl' process [] . words
     where   process (r:l:xs) "+" = (l + r):xs
             process (r:l:xs) "-" = (l - r):xs
             process (r:l:xs) "*" = (l * r):xs
             process (r:l:xs) "/" = (l / r):xs
+            process (r:l:xs) "^" = (l ** r):xs
+            process (x:xs) "ln" = log x:xs
+            process xs "sum" = [sum xs]
             process xs numberString = read numberString:xs
